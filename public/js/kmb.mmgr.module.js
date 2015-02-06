bdde3405d0d36429defd772bfbec10c9046c4a9e
Emmanuel Bouton <emmanuel.bouton@orange.com>
2015-02-06 18:55:23 +0100

    Disable btn when module is not updatable

--- public/js/kmb.mmgr.module.js
+++ public/js/kmb.mmgr.module.js
@@ -1,17 +1,19 @@
 $(window).load(function () {
-
-    $('#update-module.modal').on('show.bs.modal', function (e) {
-        $.ajax({
-            url: $(e.relatedTarget).data('version-href'),
-            dataType: "json",
-            success: function (data) {
-                var select = $("select#version");
-                select.html('');
-                $.each(data, function (index, version) {
-                    select.append($('<option></option>').html(version));
-                });
-                select.trigger('chosen:updated');
+    $.ajax({
+        url: $('#update').data('version-href'),
+        dataType: "json",
+        statusCode: {
+            404: function() {
+                $('#remove').addClass('disabled');
+                $('#update').addClass('disabled');
             }
-        });
+        },
+        success: function (data) {
+            var select = $("select#version");
+            $.each(data, function (index, version) {
+                select.append($('<option></option>').html(version));
+            });
+            select.trigger('chosen:updated');
+        }
     });
 });
