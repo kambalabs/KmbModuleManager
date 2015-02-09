2ca64e201ad367d3a71a10a77aa56f15b1d27213
Emmanuel Bouton <emmanuel.bouton@orange.com>
2015-02-09 17:01:23 +0100

    UPDATE module buttons disable by default

--- public/js/kmb.mmgr.module.js
+++ public/js/kmb.mmgr.module.js
@@ -2,18 +2,14 @@ $(window).load(function () {
     $.ajax({
         url: $('#update').data('version-href'),
         dataType: "json",
-        statusCode: {
-            404: function() {
-                $('#remove').addClass('disabled');
-                $('#update').addClass('disabled');
-            }
-        },
         success: function (data) {
             var select = $("select#version");
             $.each(data, function (index, version) {
                 select.append($('<option></option>').html(version));
             });
             select.trigger('chosen:updated');
+            $('#remove').removeClass('disabled');
+            $('#update').removeClass('disabled');
         }
     });
 });
